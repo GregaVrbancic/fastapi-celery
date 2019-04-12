94a7d3bd8950236160a8241de0eb2cbd806a0a99
Grega Vrbančič <grega.vrbancic@gmail.com>
2019-04-12 23:45:48 +0200

    working basic example with update_state callback

--- server/worker/celery_app.py
+++ server/worker/celery_app.py
@@ -2,8 +2,10 @@ from celery import Celery
 
 celery_app = Celery(
     "worker",
-    backend="amqp://user:bitnami@localhost:5672//",
+    backend="redis://:password123@localhost:6379/0",
     broker="amqp://user:bitnami@localhost:5672//"
 )
 
-celery_app.conf.task_routes = {"worker.celery_worker.test_celery": "test-queue"}
+celery_app.conf.task_routes = {
+    "worker.celery_worker.test_celery": "test-queue"}
+celery_app.conf.update(task_track_started=True)
--- server/worker/celery_worker.py
+++ server/worker/celery_worker.py
@@ -1,8 +1,14 @@
 from time import sleep
+
+from celery import current_task
+
 from .celery_app import celery_app
 
 
 @celery_app.task(acks_late=True)
 def test_celery(word: str):
-    sleep(10)
+    for i in range(1, 11):
+        sleep(1)
+        current_task.update_state(state='PROGRESS',
+                                  meta={'process_percent': i*10})
     return f"test task return {word}"
